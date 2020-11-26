dca9932dcb54061ebb6d15ce74f0937bd8e5cc40
Verackochi <48618939+Verackochi@users.noreply.github.com>
2020-11-26 12:43:16 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -12,17 +12,19 @@ $(function () {
 
 /* филтр категорий */
 $(function () {
-
+    /*переменная с масивом ссылок */
     let filter = $("[data-filter]");
-
+    /*действия при клике убирает стандартное поведение */
     filter.on("click", function (event) {
         event.preventDefault();
-
+        /*берем данные из масива в переменну */
         let cat = $(this).data('filter');
-
+        /*если переменая равна all удаляемем класс hiden( содержит display:none) у всех карточек */
         if (cat == 'all') {
             $("[data-cat]").removeClass("hide");
         } else {
+            /*проверям совпадение с другими ссылка если элемент масива ссылки совпадает то ко всем остальным блокам 
+            приметь display: none с помощью класса hiden  */
             $("[data-cat]").each(function () {
                 let workCat = $(this).data('cat');
 
