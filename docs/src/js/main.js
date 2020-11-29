5234bdb0c6243ac5dc3fa0d1ed81bff6065cf255
Verackochi <48618939+Verackochi@users.noreply.github.com>
2020-11-29 10:57:28 +0300

    3.1.1

--- docs/src/js/main.js
+++ docs/src/js/main.js
@@ -8,6 +8,44 @@ $(function () {
         starSize: 12
     });
 
+    /*слайдер*/
+
+    $('.trend__slider').slick({
+        arrows: false,
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
     /*филтр категорий*/
     function app() {
         /*переменная для масива кнопок(использует клас) */
